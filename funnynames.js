8d742121ce8c4f4deee1884f2fb7f532bdfd4238
LetGame <letgamemc@gmail.com>
2023-03-29 16:09:37 +0000

    rm cache

--- funnynames.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    "non_bomb" : (color, rank, name) => `§7${name}`,
-//	"let_game": (color, rank, name) => `${rank}§d${name}`,
-	"dYonDD": (color, rank, name) => `${rank ? '§c[§fYOUTUBE§c] ' : ''}§c${name}`
-}
--- funnynetworth.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	"let_game": (nw, numberformatter) => "ඞ " + numberformatter(nw, 3) + " ඞ",
-	"thered0": (nw, numberformatter) => "ඞ",
-//	"saulcorrea": (nw, numberformatter) => numberformatter(nw / 16, 3)
-}
